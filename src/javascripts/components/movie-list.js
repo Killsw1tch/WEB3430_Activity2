c3d0fb30853a2027e56d04037fe7b6a304130c75
Hunter Wille <48933001+Killsw1tch@users.noreply.github.com>
2021-01-25 14:47:19 -0700

    initial commit

---
+++ src/javascripts/components/movie-list.js
@@ -0,0 +1,40 @@
+import React from 'react'
+export class MovieList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {movies: this.props.movies}
+        this.sortMovies = this.sortMovies.bind(this)
+    }
+
+    sortMovies(){
+        this.state.movies.sort((a, b) => {
+          return a.votes - b.votes
+        })
+        this.setState({
+          movies: this.state.movies
+        })
+    }
+
+    render(){
+      return(
+        <div className="container">
+          <header>
+            <h1>Top 10 Movies: Hunter Wille</h1>
+          </header>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>List</li>
+              <li>About</li>
+            </ul>
+            <button className="primary" onClick={this.sortMovies}>Sort</button>
+          </nav>
+          <main>
+            {this.state.movies.map(m => {
+              return <Movie key={m.id} movie={m}/>
+            })}
+          </main>
+        </div>
+      )
+    }
+  }
